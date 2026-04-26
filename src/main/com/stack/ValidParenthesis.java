442f2085a5307dfa12039c18d848fd632b9560c9
Dhruv-0109 <74096335+Dhruv-0109@users.noreply.github.com>
2026-04-26 02:07:45 +0000

    LC 20 - Valid Parenthesis-Stack

---
+++ src/main/com/stack/ValidParenthesis.java
@@ -0,0 +1,38 @@
+public class ValidParenthesis {
+    public boolean isValid(String s) {
+
+        if(s.isEmpty()) return true;
+
+        int len = s.length();
+        Stack <Character> stack = new Stack<>();
+
+        for(int i=0; i < len; i++)
+        {
+            char c = s.charAt(i);
+
+            if(isOpenBracket(c))
+                stack.push(c);
+            else{
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if(!isMatching(top,c))
+                    return false;
+            }   
+        }
+        return stack.isEmpty(); //if all characters are popped - i.e matching brackets are found
+    }
+
+    public boolean isOpenBracket(char c)
+    {
+        return c=='(' || c == '{' || c == '[';
+    }
+
+    public boolean isMatching(char open, char close)
+    {
+        if(open == '(' && close == ')') return true;
+        if(open == '[' && close == ']') return true;
+        if(open == '{' && close == '}') return true;
+        else
+            return false;
+    }
+}
